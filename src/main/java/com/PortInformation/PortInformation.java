aef85a6e7e4ab0f1330f52cbde73a7a1a60e57cc
Sai Kiran <saisarve@in.ibm.com>
2017-07-13 19:45:22 +0530

    Initial commit

---
+++ src/main/java/com/PortInformation/PortInformation.java
@@ -0,0 +1,448 @@
+package com.PortInformation;
+import java.util.ArrayList;
+
+
+
+
+public class PortInformation {
+	
+	public String nativeIpValue;
+	public boolean nativeIpIsUpdatable;
+	public boolean nativeIPIsRequired;
+	public boolean nativeIPIsDisplayed;
+	public String serviceNameValue;
+	public boolean serviceNameisUpdatable;
+	public boolean serviceNameisRequired;
+	public boolean serviceNameIPIsDisplayed;
+	public ArrayList<String> actionDropdownList = new ArrayList<String>(20);
+	public boolean actionIsUpdatable;
+	public boolean actionIsRequired;
+	public boolean actionIsDisplayed;
+	public String actionSelectedValue;
+	public ArrayList<String> portSpeedDropdownList = new ArrayList<String>(20);
+	public boolean portSpeedIsUpdatable;
+	public boolean portSpeedIsRequired;
+	public boolean portSpeedIsDisplayed;
+	public boolean portSpeedIsSelected;
+	public String portSpeedSelectedValue;
+	public ArrayList<String> portBillingOptionDropdownList = new ArrayList<String>(20);
+	public boolean portBillingOptionIsUpdatable;
+	public boolean portBillingOptionIsRequired;
+	public boolean portBillingOptionIsDisplayed;
+	public String portBillingOptionSelectedValue;
+	public ArrayList<String> accessMethodTypeDropdownList = new ArrayList<String>(20);
+	public boolean accessMethodTypeIsUpdatable;
+	public boolean accessMethodTypeIsRequired;
+	public boolean accessMethodTypeIsDisplayed;
+	public String accessMethodTypeSelectedValue;
+	public ArrayList<String> interconnectTypeDropdownList = new ArrayList<String>(20);
+	public boolean interconnectTypeIsUpdatable;
+	public boolean interconnectTypeIsRequired;
+	public boolean interconnectTypeIsDisplayed;
+	public String interconnectTypeSelectedValue;
+	public String serviceValue;
+	public boolean serviceIsUpdatable;
+	public boolean serviceIsRequired;
+	public boolean serviceIsSelected;
+	public boolean serviceIsDisplayed;
+	public String serviceAreaValue;
+	public boolean serviceAreaIsUpdatable;
+	public boolean serviceAreaIsRequired;
+	public boolean serviceAreaIsSelected;
+	public boolean serviceAreaIsDisplayed;
+	public boolean etherNetAreaValue;
+	public boolean ethernetIsUpdatable;
+	public boolean ethernetIsRequired;
+	public boolean ethernetIsSelected;
+	public boolean ethernetIsDisplayed;
+	public ArrayList<String> serviceActivityDropdownList = new ArrayList<String>(20);
+	public boolean serviceActivityIsUpdatable;
+	public boolean serviceActivityIsRequired;
+	public boolean serviceActivityIsSelected;
+	public boolean serviceActivityIsDisplayed;
+	public String serviceActivitySelectedValue;
+	public ArrayList<String> avpnManagementDropdownList = new ArrayList<String>(20);
+	public boolean avpnManagementIsUpdatable;
+	public boolean avpnManagementIsRequired;
+	public boolean avpnManagementIsSelected;
+	public boolean avpnManagementIsDisplayed;
+	public String avpnManagementSelectedValue;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getNativeIpValue() {
+		return nativeIpValue;
+	}
+	public boolean isNativeIpIsUpdatable() {
+		return nativeIpIsUpdatable;
+	}
+	public boolean isNativeIPIsRequired() {
+		return nativeIPIsRequired;
+	}
+	public boolean isNativeIPIsDisplayed() {
+		return nativeIPIsDisplayed;
+	}
+	public String getServiceNameValue() {
+		return serviceNameValue;
+	}
+	public boolean isServiceNameisUpdatable() {
+		return serviceNameisUpdatable;
+	}
+	public boolean isServiceNameisRequired() {
+		return serviceNameisRequired;
+	}
+	public boolean isServiceNameIPIsDisplayed() {
+		return serviceNameIPIsDisplayed;
+	}
+	public ArrayList<String> getActionDropdownList() {
+		return actionDropdownList;
+	}
+	public boolean isActionIsUpdatable() {
+		return actionIsUpdatable;
+	}
+	public boolean isActionIsRequired() {
+		return actionIsRequired;
+	}
+	public boolean isActionIsDisplayed() {
+		return actionIsDisplayed;
+	}
+	public ArrayList<String> getPortSpeedDropdownList() {
+		return portSpeedDropdownList;
+	}
+	public boolean isPortSpeedIsUpdatable() {
+		return portSpeedIsUpdatable;
+	}
+	public boolean isPortSpeedIsRequired() {
+		return portSpeedIsRequired;
+	}
+	public boolean isPortSpeedIsDisplayed() {
+		return portSpeedIsDisplayed;
+	}
+	public boolean isPortSpeedIsSelected() {
+		return portSpeedIsSelected;
+	}
+	public ArrayList<String> getPortBillingOptionDropdownList() {
+		return portBillingOptionDropdownList;
+	}
+	public boolean isPortBillingOptionIsUpdatable() {
+		return portBillingOptionIsUpdatable;
+	}
+	public boolean isPortBillingOptionIsRequired() {
+		return portBillingOptionIsRequired;
+	}
+	public boolean isPortBillingOptionIsDisplayed() {
+		return portBillingOptionIsDisplayed;
+	}
+	public ArrayList<String> getAccessMethodTypeDropdownList() {
+		return accessMethodTypeDropdownList;
+	}
+	public boolean isAccessMethodTypeIsUpdatable() {
+		return accessMethodTypeIsUpdatable;
+	}
+	public boolean isAccessMethodTypeIsRequired() {
+		return accessMethodTypeIsRequired;
+	}
+	public boolean isAccessMethodTypeIsDisplayed() {
+		return accessMethodTypeIsDisplayed;
+	}
+	public ArrayList<String> getInterconnectTypeDropdownList() {
+		return interconnectTypeDropdownList;
+	}
+	public boolean isInterconnectTypeIsUpdatable() {
+		return interconnectTypeIsUpdatable;
+	}
+	public boolean isInterconnectTypeIsRequired() {
+		return interconnectTypeIsRequired;
+	}
+	public boolean isInterconnectTypeIsDisplayed() {
+		return interconnectTypeIsDisplayed;
+	}
+	public String getActionSelectedValue() {
+		return actionSelectedValue;
+	}
+	public String getPortSpeedSelectedValue() {
+		return portSpeedSelectedValue;
+	}
+	public String getPortBillingOptionSelectedValue() {
+		return portBillingOptionSelectedValue;
+	}
+	public String getAccessMethodTypeSelectedValue() {
+		return accessMethodTypeSelectedValue;
+	}
+	public String getInterconnectTypeSelectedValue() {
+		return interconnectTypeSelectedValue;
+	}
+	public String getServiceValue() {
+		return serviceValue;
+	}
+	public boolean isServiceIsUpdatable() {
+		return serviceIsUpdatable;
+	}
+	public boolean isServiceIsRequired() {
+		return serviceIsRequired;
+	}
+	public boolean isServiceIsSelected() {
+		return serviceIsSelected;
+	}
+	public boolean isServiceIsDisplayed() {
+		return serviceIsDisplayed;
+	}
+	public String getServiceAreaValue() {
+		return serviceAreaValue;
+	}
+	public boolean isServiceAreaIsUpdatable() {
+		return serviceAreaIsUpdatable;
+	}
+	public boolean isServiceAreaIsRequired() {
+		return serviceAreaIsRequired;
+	}
+	public boolean isServiceAreaIsSelected() {
+		return serviceAreaIsSelected;
+	}
+	public boolean isServiceAreaIsDisplayed() {
+		return serviceAreaIsDisplayed;
+	}
+	public boolean getEtherNetAreaValue() {
+		return etherNetAreaValue;
+	}
+	public boolean isEthernetIsUpdatable() {
+		return ethernetIsUpdatable;
+	}
+	public boolean isEthernetIsRequired() {
+		return ethernetIsRequired;
+	}
+	public boolean isEthernetIsSelected() {
+		return ethernetIsSelected;
+	}
+	public boolean isEthernetIsDisplayed() {
+		return ethernetIsDisplayed;
+	}
+	public ArrayList<String> getServiceActivityDropdownList() {
+		return serviceActivityDropdownList;
+	}
+	public boolean isServiceActivityIsUpdatable() {
+		return serviceActivityIsUpdatable;
+	}
+	public boolean isServiceActivityIsRequired() {
+		return serviceActivityIsRequired;
+	}
+	public boolean isServiceActivityIsSelected() {
+		return serviceActivityIsSelected;
+	}
+	public boolean isServiceActivityIsDisplayed() {
+		return serviceActivityIsDisplayed;
+	}
+	public String getServiceActivitySelectedValue() {
+		return serviceActivitySelectedValue;
+	}
+	public ArrayList<String> getAvpnManagementDropdownList() {
+		return avpnManagementDropdownList;
+	}
+	public boolean isAvpnManagementIsUpdatable() {
+		return avpnManagementIsUpdatable;
+	}
+	public boolean isAvpnManagementIsRequired() {
+		return avpnManagementIsRequired;
+	}
+	public boolean isAvpnManagementIsSelected() {
+		return avpnManagementIsSelected;
+	}
+	public boolean isAvpnManagementIsDisplayed() {
+		return avpnManagementIsDisplayed;
+	}
+	public String getAvpnManagementSelectedValue() {
+		return avpnManagementSelectedValue;
+	}
+	public void setServiceValue(String serviceValue) {
+		this.serviceValue = serviceValue;
+	}
+	public void setServiceIsUpdatable(boolean serviceIsUpdatable) {
+		this.serviceIsUpdatable = serviceIsUpdatable;
+	}
+	public void setServiceIsRequired(boolean serviceIsRequired) {
+		this.serviceIsRequired = serviceIsRequired;
+	}
+	public void setServiceIsSelected(boolean serviceIsSelected) {
+		this.serviceIsSelected = serviceIsSelected;
+	}
+	public void setServiceIsDisplayed(boolean serviceIsDisplayed) {
+		this.serviceIsDisplayed = serviceIsDisplayed;
+	}
+	public void setServiceAreaValue(String serviceAreaValue) {
+		this.serviceAreaValue = serviceAreaValue;
+	}
+	public void setServiceAreaIsUpdatable(boolean serviceAreaIsUpdatable) {
+		this.serviceAreaIsUpdatable = serviceAreaIsUpdatable;
+	}
+	public void setServiceAreaIsRequired(boolean serviceAreaIsRequired) {
+		this.serviceAreaIsRequired = serviceAreaIsRequired;
+	}
+	public void setServiceAreaIsSelected(boolean serviceAreaIsSelected) {
+		this.serviceAreaIsSelected = serviceAreaIsSelected;
+	}
+	public void setServiceAreaIsDisplayed(boolean serviceAreaIsDisplayed) {
+		this.serviceAreaIsDisplayed = serviceAreaIsDisplayed;
+	}
+	public void setEtherNetAreaValue(boolean etherNetAreaValue) {
+		this.etherNetAreaValue = etherNetAreaValue;
+	}
+	public void setEthernetIsUpdatable(boolean ethernetIsUpdatable) {
+		this.ethernetIsUpdatable = ethernetIsUpdatable;
+	}
+	public void setEthernetIsRequired(boolean ethernetIsRequired) {
+		this.ethernetIsRequired = ethernetIsRequired;
+	}
+	public void setEthernetIsSelected(boolean ethernetIsSelected) {
+		this.ethernetIsSelected = ethernetIsSelected;
+	}
+	public void setEthernetIsDisplayed(boolean ethernetIsDisplayed) {
+		this.ethernetIsDisplayed = ethernetIsDisplayed;
+	}
+	public void setServiceActivityDropdownList(ArrayList<String> serviceActivityDropdownList) {
+		this.serviceActivityDropdownList = serviceActivityDropdownList;
+	}
+	public void setServiceActivityIsUpdatable(boolean serviceActivityIsUpdatable) {
+		this.serviceActivityIsUpdatable = serviceActivityIsUpdatable;
+	}
+	public void setServiceActivityIsRequired(boolean serviceActivityIsRequired) {
+		this.serviceActivityIsRequired = serviceActivityIsRequired;
+	}
+	public void setServiceActivityIsSelected(boolean serviceActivityIsSelected) {
+		this.serviceActivityIsSelected = serviceActivityIsSelected;
+	}
+	public void setServiceActivityIsDisplayed(boolean serviceActivityIsDisplayed) {
+		this.serviceActivityIsDisplayed = serviceActivityIsDisplayed;
+	}
+	public void setServiceActivitySelectedValue(String serviceActivitySelectedValue) {
+		this.serviceActivitySelectedValue = serviceActivitySelectedValue;
+	}
+	public void setAvpnManagementDropdownList(ArrayList<String> avpnManagementDropdownList) {
+		this.avpnManagementDropdownList = avpnManagementDropdownList;
+	}
+	public void setAvpnManagementIsUpdatable(boolean avpnManagementIsUpdatable) {
+		this.avpnManagementIsUpdatable = avpnManagementIsUpdatable;
+	}
+	public void setAvpnManagementIsRequired(boolean avpnManagementIsRequired) {
+		this.avpnManagementIsRequired = avpnManagementIsRequired;
+	}
+	public void setAvpnManagementIsSelected(boolean avpnManagementIsSelected) {
+		this.avpnManagementIsSelected = avpnManagementIsSelected;
+	}
+	public void setAvpnManagementIsDisplayed(boolean avpnManagementIsDisplayed) {
+		this.avpnManagementIsDisplayed = avpnManagementIsDisplayed;
+	}
+	public void setAvpnManagementSelectedValue(String avpnManagementSelectedValue) {
+		this.avpnManagementSelectedValue = avpnManagementSelectedValue;
+	}
+	public void setActionSelectedValue(String actionSelectedValue) {
+		this.actionSelectedValue = actionSelectedValue;
+	}
+	public void setPortSpeedSelectedValue(String portSpeedSelectedValue) {
+		this.portSpeedSelectedValue = portSpeedSelectedValue;
+	}
+	public void setPortBillingOptionSelectedValue(String portBillingOptionSelectedValue) {
+		this.portBillingOptionSelectedValue = portBillingOptionSelectedValue;
+	}
+	public void setAccessMethodTypeSelectedValue(String accessMethodTypeSelectedValue) {
+		this.accessMethodTypeSelectedValue = accessMethodTypeSelectedValue;
+	}
+	public void setInterconnectTypeSelectedValue(String interconnectTypeSelectedValue) {
+		this.interconnectTypeSelectedValue = interconnectTypeSelectedValue;
+	}
+	public void setNativeIpValue(String nativeIpValue) {
+		this.nativeIpValue = nativeIpValue;
+	}
+	public void setNativeIpIsUpdatable(boolean nativeIpIsUpdatable) {
+		this.nativeIpIsUpdatable = nativeIpIsUpdatable;
+	}
+	public void setNativeIPIsRequired(boolean nativeIPIsRequired) {
+		this.nativeIPIsRequired = nativeIPIsRequired;
+	}
+	public void setNativeIPIsDisplayed(boolean nativeIPIsDisplayed) {
+		this.nativeIPIsDisplayed = nativeIPIsDisplayed;
+	}
+	public void setServiceNameValue(String serviceNameValue) {
+		this.serviceNameValue = serviceNameValue;
+	}
+	public void setServiceNameisUpdatable(boolean serviceNameisUpdatable) {
+		this.serviceNameisUpdatable = serviceNameisUpdatable;
+	}
+	public void setServiceNameisRequired(boolean serviceNameisRequired) {
+		this.serviceNameisRequired = serviceNameisRequired;
+	}
+	public void setServiceNameIPIsDisplayed(boolean serviceNameIPIsDisplayed) {
+		this.serviceNameIPIsDisplayed = serviceNameIPIsDisplayed;
+	}
+	public void setActionDropdownList(ArrayList<String> actionDropdownList) {
+		this.actionDropdownList = actionDropdownList;
+	}
+	public void setActionIsUpdatable(boolean actionIsUpdatable) {
+		this.actionIsUpdatable = actionIsUpdatable;
+	}
+	public void setActionIsRequired(boolean actionIsRequired) {
+		this.actionIsRequired = actionIsRequired;
+	}
+	public void setActionIsDisplayed(boolean actionIsDisplayed) {
+		this.actionIsDisplayed = actionIsDisplayed;
+	}
+	public void setPortSpeedDropdownList(ArrayList<String> portSpeedDropdownList) {
+		this.portSpeedDropdownList = portSpeedDropdownList;
+	}
+	public void setPortSpeedIsUpdatable(boolean portSpeedIsUpdatable) {
+		this.portSpeedIsUpdatable = portSpeedIsUpdatable;
+	}
+	public void setPortSpeedIsRequired(boolean portSpeedIsRequired) {
+		this.portSpeedIsRequired = portSpeedIsRequired;
+	}
+	public void setPortSpeedIsDisplayed(boolean portSpeedIsDisplayed) {
+		this.portSpeedIsDisplayed = portSpeedIsDisplayed;
+	}
+	public void setPortSpeedIsSelected(boolean portSpeedIsSelected) {
+		this.portSpeedIsSelected = portSpeedIsSelected;
+	}
+	public void setPortBillingOptionDropdownList(ArrayList<String> portBillingOptionDropdownList) {
+		this.portBillingOptionDropdownList = portBillingOptionDropdownList;
+	}
+	public void setPortBillingOptionIsUpdatable(boolean portBillingOptionIsUpdatable) {
+		this.portBillingOptionIsUpdatable = portBillingOptionIsUpdatable;
+	}
+	public void setPortBillingOptionIsRequired(boolean portBillingOptionIsRequired) {
+		this.portBillingOptionIsRequired = portBillingOptionIsRequired;
+	}
+	public void setPortBillingOptionIsDisplayed(boolean portBillingOptionIsDisplayed) {
+		this.portBillingOptionIsDisplayed = portBillingOptionIsDisplayed;
+	}
+	public void setAccessMethodTypeDropdownList(ArrayList<String> accessMethodTypeDropdownList) {
+		this.accessMethodTypeDropdownList = accessMethodTypeDropdownList;
+	}
+	public void setAccessMethodTypeIsUpdatable(boolean accessMethodTypeIsUpdatable) {
+		this.accessMethodTypeIsUpdatable = accessMethodTypeIsUpdatable;
+	}
+	public void setAccessMethodTypeIsRequired(boolean accessMethodTypeIsRequired) {
+		this.accessMethodTypeIsRequired = accessMethodTypeIsRequired;
+	}
+	public void setAccessMethodTypeIsDisplayed(boolean accessMethodTypeIsDisplayed) {
+		this.accessMethodTypeIsDisplayed = accessMethodTypeIsDisplayed;
+	}
+	public void setInterconnectTypeDropdownList(ArrayList<String> interconnectTypeDropdownList) {
+		this.interconnectTypeDropdownList = interconnectTypeDropdownList;
+	}
+	public void setInterconnectTypeIsUpdatable(boolean interconnectTypeIsUpdatable) {
+		this.interconnectTypeIsUpdatable = interconnectTypeIsUpdatable;
+	}
+	public void setInterconnectTypeIsRequired(boolean interconnectTypeIsRequired) {
+		this.interconnectTypeIsRequired = interconnectTypeIsRequired;
+	}
+	public void setInterconnectTypeIsDisplayed(boolean interconnectTypeIsDisplayed) {
+		this.interconnectTypeIsDisplayed = interconnectTypeIsDisplayed;
+	}
+	
+		
+}
---
+++ src/main/java/com/Test/TestFile.java
@@ -0,0 +1,40 @@
+package com.Test;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import com.PortInformation.PortInformation;
+
+public class TestFile {
+    
+
+	public static void main(String[] args) {
+		try{
+			
+			 KieServices ks = KieServices.Factory.get();
+			 System.out.println("atk session");
+	    	KieContainer kContainer = ks.getKieClasspathContainer();
+	    	System.out.println("atk sessio n2  ");
+	        KieSession kSession = kContainer.newKieSession("ksession-process");
+	        System.out.println("atk sessio n3  ");
+	        PortInformation  PortObject = new PortInformation();
+	        System.out.println("atk sessio n4  ");
+	        System.out.println("Executing the results");
+	        PortObject.setPortSpeedIsSelected(true);
+	        PortObject.setPortBillingOptionSelectedValue("Highcap Flex");
+	        kSession.insert(PortObject);
+	        
+	        kSession.fireAllRules();
+	        System.out.println("Executed the results");
+	        System.out.println(PortObject.getNativeIpValue());
+	        System.out.println(PortObject.getPortBillingOptionDropdownList());
+			
+		}catch(Exception e){
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
